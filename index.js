0b5c96202408f0d73824a8b587150ec0a9c4977f
Chris Mimm <mimmdev@gmail.com>
2017-10-26 23:17:55 +0100

    :(

---
+++ index.js
@@ -0,0 +1,45 @@
+var app = {
+    initialize: function() {
+        this.bindEvents()
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false)
+    },
+
+    onDeviceReady: function() {
+        alert("Start message");
+        app.receivedEvent('deviceready');
+        nfc.addNdefListener(
+          app.onNdef,
+          function() {
+            console.log("Success.");
+          },
+          function() {
+            console.log("Fail.");
+          }
+        );
+    },
+
+    receivedEvent: function(id) {
+    },
+
+    onNdef: function(nfcEvent) {
+        console.log(JSON.stringify(nfcEvent.tag));
+        alert("NFC read");
+    }
+
+}
+
+
+
+function parseTag(nfcEvent) {
+  var records = nfcEvent.tagData;
+
+  for (var i = 0; i < records.length; i++) {
+    var record = records[i],
+    p = document.createElement('p');
+    p.innerHTML = nfc.bytesToString(record.payload);
+    display.appendChild(p);
+  }
+}
